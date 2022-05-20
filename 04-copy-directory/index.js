a6c95a91e8b1692c5ade0f5aaf5e5b9b103e42df
Gegcbr9319 <gegcbr9319@gmail.com>
2022-05-20 22:36:50 +0300

    feat: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,34 +1,34 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs/promises');
+
 
 const filess = path.join(__dirname, 'files');
 const filesCopy = path.join(__dirname, 'files-copy');
 
-fs.rm(filesCopy,{recursive:true}, (err) => {
-  if(err) throw err;
-  console.log('Folder deleted');
+async function copyDir(){
+  await fs.mkdir(filesCopy,{recursive:true});
+  const files =  await fs.readdir(filess,{withFileTypes:true});
+  files.forEach( async file => {
+    if (await file.isFile()) {
+      const a = `${filess}/${file.name}`;
+      const b = `${filesCopy}/${file.name}`;
+      await fs.copyFile(a,b);
+    }
     
-});
-
-fs.mkdir(filesCopy,{recursive:true}, (err) =>{
-  if(err) throw err;
-  else {
-    fs.readdir(filess,{withFileTypes: true}, (err, files) =>{
-      if(err) throw err;
-      else{
-        files.forEach(file => {
-          if (file.isFile()) {
-            const a = `${filess}/${file.name}`;
-            const b = `${filesCopy}/${file.name}`;
-            fs.copyFile(a,b, err =>{
-              if(err) throw err;
-            });
-          }
-        });    
-      }
-    
-    });    
-  }
-});
+  });    
+}
 
 
+
+async function delDir(){
+  await fs.rm(filesCopy,{recursive:true, force:true});
+}
+
+ 
+async function create(){
+  
+  await delDir();
+  await copyDir(); 
+}
+
+create(); 
